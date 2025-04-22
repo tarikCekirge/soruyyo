fb4a13dd31d04dd838613fa9ed9656cbdf0481d1
Tarık Çekirge <tcekirge@gmail.com>
2025-04-22 21:56:40 +0300

    axiosInstance  update

--- frontend/src/utils/axiosInstance.tsx
+++ frontend/src/utils/axiosInstance.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { BASE_URL } from './apiPaths'
+import { toast } from 'sonner';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 2000,
+    timeout: 3000,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
@@ -27,21 +28,33 @@ axiosInstance.interceptors.request.use(
 // Response Interceptor
 axiosInstance.interceptors.response.use(
     (response) => {
-        return response
+        return response;
     },
     (error) => {
         if (error.response) {
-            if (error.response.status === 401) {
-                console.error("Unauthorized! Redirecting to login");
-                window.location.href = "/login"
-            } else if (error.response.status === 500) {
-                console.error("Server error.");
-            } else if (error.code === "ECONNABORTED") {
-                console.error("Request timeout.");
+            switch (error.response.status) {
+                case 401:
+
+                    console.error("Unauthorized! Redirecting to login");
+                    toast.error("Yetkisiz! Girişe yönlendiriliyorsunuz")
+                    window.location.href = "/login";
+                    break;
+                case 500:
+                    console.error("Server error.");
+                    toast.error("Sunucu haatası")
+                    break;
+                default:
+                    console.error(`Error: ${error.response.status}`);
+                    toast.error(`${error.response.status || "Beklenmeyen bir hata oldu"}`)
+                    break;
             }
+        } else if (error.code === "ECONNABORTED") {
+            console.error("Request timeout.");
+            toast.error("Zaman aşımı")
         }
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
 )
+
 
 export default axiosInstance
